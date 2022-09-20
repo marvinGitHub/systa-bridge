bedcea23ceac0eb3a66e67323d6a727e388f3642
Marvin Elia Hoppe <marvin.hoppe@coxautoinc.com>
2022-09-20 09:59:53 +0200

    Fix: plugin

--- src/PluginOperationTimeBoiler.php
+++ src/PluginOperationTimeBoiler.php
@@ -4,9 +4,8 @@ class PluginOperationTimeBoiler extends PluginAbstract
 {
     use IntervalAwareTrait;
 
-    const STORAGE_KEY_TEMPERATURE_FLOW_BOILER_MIN = 'PluginOperationTimeBoiler.temperatureFlowBoilerMin';
-    const STORAGE_KEY_TEMPERATURE_FLOW_BOILER_MAX = 'PluginOperationTimeBoiler.temperatureFlowBoilerMax';
-    const STORAGE_KEY_TIMESTAMP_START = 'PluginOperationTimeBoiler.timestampStart';
+    private $temperatures = [];
+
     const STORAGE_KEY_TIMESTAMP_NEXT_EVALUATION = 'PluginOperationTimeBoiler.timestampNextEvaluation';
 
     protected function getIntervalDefault(): int
@@ -16,15 +15,20 @@ class PluginOperationTimeBoiler extends PluginAbstract
 
     private function init(PluginContext $context)
     {
-        $context->getStorage()->set(PluginOperationTimeBoiler::STORAGE_KEY_TIMESTAMP_START, time());
+        $this->temperatures = [];
+
         $context->getStorage()->set(PluginOperationTimeBoiler::STORAGE_KEY_TIMESTAMP_NEXT_EVALUATION, time());
-        $context->getStorage()->set(PluginOperationTimeBoiler::STORAGE_KEY_TEMPERATURE_FLOW_BOILER_MAX, $context->getMonitor()->getTemperatureFlowBoiler());
-        $context->getStorage()->set(PluginOperationTimeBoiler::STORAGE_KEY_TEMPERATURE_FLOW_BOILER_MIN, $context->getMonitor()->getTemperatureFlowBoiler());
+    }
+
+    private function getTemperatureMax(): int
+    {
+        return end($this->temperatures) ?? 0;
     }
 
     public function run(PluginContext $context)
     {
         $storage = $context->getStorage();
+
         $timestampNextEvaluation = $storage->get(PluginOperationTimeBoiler::STORAGE_KEY_TIMESTAMP_NEXT_EVALUATION);
 
         if (null === $timestampNextEvaluation) {
@@ -40,13 +44,31 @@ class PluginOperationTimeBoiler extends PluginAbstract
 
         $temperatureFlowBoilerCurrent = $context->getMonitor()->getTemperatureFlowBoiler();
 
-        if ($temperatureFlowBoilerCurrent < $storage->get(PluginOperationTimeBoiler::STORAGE_KEY_TEMPERATURE_FLOW_BOILER_MIN)) {
-            $storage->set(PluginOperationTimeBoiler::STORAGE_KEY_TEMPERATURE_FLOW_BOILER_MIN, $temperatureFlowBoilerCurrent);
-            $storage->set(PluginOperationTimeBoiler::STORAGE_KEY_TIMESTAMP_START, time());
-        } elseif ($temperatureFlowBoilerCurrent > $storage->get(PluginOperationTimeBoiler::STORAGE_KEY_TEMPERATURE_FLOW_BOILER_MAX)) {
-            $storage->set(PluginOperationTimeBoiler::STORAGE_KEY_TEMPERATURE_FLOW_BOILER_MAX, $temperatureFlowBoilerCurrent);
-            $operationTimeMinutes = (time() - $storage->get(PluginOperationTimeBoiler::STORAGE_KEY_TIMESTAMP_START)) / 60;
-            $context->getMonitor()->set('operationTimeMinutesBoiler', (int)$operationTimeMinutes);
+        if (!$temperatureFlowBoilerCurrent) {
+            return;
         }
+
+        $this->temperatures[$temperatureFlowBoilerCurrent] = time();
+        ksort($this->temperatures);
+
+        if ($temperatureFlowBoilerCurrent >= $this->getTemperatureMax()) {
+            return; // peak not reached yet
+        }
+
+        $start = array_shift($this->temperatures);
+        if (empty($start)) {
+            return;
+        }
+
+        $end = array_pop($this->temperatures);
+        if (empty($end)) {
+            return;
+        }
+
+        $operationTimeMinutes = ($end - $start) / 60;
+
+        $context->getMonitor()->set('operationTimeMinutesBoiler', (int)$operationTimeMinutes);
+
+        $this->init($context);
     }
 }
