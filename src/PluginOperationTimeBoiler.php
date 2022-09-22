fd68c86eab83b5f432c1ef100e12f9407b59d6d4
Marvin Elia Hoppe <marvin.hoppe@coxautoinc.com>
2022-09-22 10:21:57 +0200

    Fix: plugin

--- src/PluginOperationTimeBoiler.php
+++ src/PluginOperationTimeBoiler.php
@@ -19,12 +19,7 @@ class PluginOperationTimeBoiler extends PluginAbstract
 
     public function run(PluginContext $context)
     {
-        if (null === $this->timestampNextEvaluation) {
-            $this->timestampNextEvaluation = time();
-            return;
-        }
-
-        if (time() < $this->timestampNextEvaluation) {
+        if (time() < (int)$this->timestampNextEvaluation) {
             return;
         }
 
@@ -39,24 +34,28 @@ class PluginOperationTimeBoiler extends PluginAbstract
         if (empty($this->temperatureMin) || $temperatureCurrent <= $this->temperatureMin) {
             $this->temperatureMin = $temperatureCurrent;
             $this->timestampTemperatureMin = time();
+            return;
         }
 
         if (empty($this->temperatureMax) || $temperatureCurrent >= $this->temperatureMax) {
             $this->temperatureMax = $temperatureCurrent;
             $this->timestampTemperatureMax = time();
+            return;
         }
 
-        if ($temperatureCurrent + PluginOperationTimeBoiler::TOLERANCE_TEMPERATURE_FLOW_BOILER < $this->temperatureMax) {
-            $operationTimeMinutes = ($this->timestampTemperatureMax - $this->timestampTemperatureMin) / 60;
-
-            $context->getMonitor()->set('timestampOperationTimeStart', $this->timestampTemperatureMin);
-            $context->getMonitor()->set('timestampOperationTimeEnd', $this->timestampTemperatureMax);
-            $context->getMonitor()->set('operationTimeMinutesBoiler', (int)$operationTimeMinutes);
-
-            $this->temperatureMin = null;
-            $this->temperatureMax = null;
-            $this->timestampTemperatureMin = null;
-            $this->timestampTemperatureMax = null;
+        if ($temperatureCurrent + PluginOperationTimeBoiler::TOLERANCE_TEMPERATURE_FLOW_BOILER >= $this->temperatureMax) {
+            return;
         }
+
+        $operationTimeMinutes = ($this->timestampTemperatureMax - $this->timestampTemperatureMin) / 60;
+
+        $context->getMonitor()->set('timestampOperationTimeStart', $this->timestampTemperatureMin);
+        $context->getMonitor()->set('timestampOperationTimeEnd', $this->timestampTemperatureMax);
+        $context->getMonitor()->set('operationTimeMinutesBoiler', (int)$operationTimeMinutes);
+
+        $this->temperatureMin = null;
+        $this->temperatureMax = null;
+        $this->timestampTemperatureMin = null;
+        $this->timestampTemperatureMax = null;
     }
 }
